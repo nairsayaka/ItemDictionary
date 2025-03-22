02d561d47a32f4899a52e59f5d728bcb48c7fef0
nairsayaka <nairsayaka@gmail.com>
2025-03-22 11:10:13 +0900

    second commit

---
+++ src/main/java/com/example/demo/ItemDictionaryHsdApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemDictionaryHsdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemDictionaryHsdApplication.class, args);
+	}
+
+}
