5481d92156df83ea9c29461422dacf75707f76cc
nairsayaka <nairsayaka@gmail.com>
2025-03-30 19:48:15 +0900

    hello world

---
+++ src/main/java/com/itemDictionary/demo/ItemDictionaryApplication.java
@@ -0,0 +1,13 @@
+package com.itemDictionary.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemDictionaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemDictionaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itemDictionary/demo/ItemDictionaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itemDictionary.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemDictionaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
